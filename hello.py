ae5847334a2226064f644515679608e934b19f94
Ilya Veremeev <vel.veremeev@gmail.com>
2023-01-16 15:13:24 +0300

    replace content type

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def application(environ, start_response):
     body = environ['QUERY_STRING'].replace('&', '\n').encode()
     status = '200 OK'
     headers = [
-        ('Content-type', 'type/plain'),
+        ('Content-type', 'text/plain'),
     ]
     start_response(status, headers)
     return body
