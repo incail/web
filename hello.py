03946b71dcacf36de8cd463bb7939b8f680b36b5
Ilya Veremeev <vel.veremeev@gmail.com>
2023-01-16 16:11:14 +0300

    last

--- hello.py
+++ hello.py
@@ -1,6 +1,4 @@
-def application(environ, start_response):
-    body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
-    status = '200 OK'
-    headers = [('Content-Type', 'text/plain')]
-    start_response(status, headers)
-    return body
+def app(environ, start_response):
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return [bytes('\r\n'.join(environ['QUERY_STRING'].split('&')),
+                  encoding="utf8")]
--- init.sh
+++ init.sh
@@ -1,2 +1,7 @@
+sudo rm -rf /etc/nginx/sites-enabled/default
+
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/nginx_2_1_11.conf
+
 sudo /etc/init.d/nginx restart
-gunicorn --bind='0.0.0.0:8080' hello:test
+
+gunicorn -w 2 -c /home/box/web/etc/hello.py hello:app & gunicorn -w 2 -c /home/box/web/etc/qa.py ask.wsgi:application & curl -vv 'http://127.0.0.1:8000/login/'
