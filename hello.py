2cfaf931c6b8f16de4390e6146954ef317e8b46b
Ilya Veremeev <vel.veremeev@gmail.com>
2023-01-16 15:51:08 +0300

    v

--- hello.py
+++ hello.py
@@ -1,8 +1,6 @@
 def application(environ, start_response):
     body = [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     status = '200 OK'
-    headers = [
-        ('Content-type', 'text/plain'),
-    ]
+    headers = [('Content-Type', 'text/plain')]
     start_response(status, headers)
     return body
