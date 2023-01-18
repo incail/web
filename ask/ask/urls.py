6b0f390ee7c49d2d37b83287b6d436b79faefc97
Ilya Veremeev <vel.veremeev@gmail.com>
2023-01-18 18:27:58 +0300

    a

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -16,12 +16,21 @@ Including another URLconf
 from django.urls import path
 from qa import views
 
-urlpatterns = [
-    path('', views.test),
-    path('login/', views.test),
-    path('signup/', views.test),
-    path('question/<int:id>/', views.test),
-    path('ask/', views.test),
-    path('popular/', views.test),
-    path('new/', views.test),
-]
+# urlpatterns = [
+#     path('', views.test),
+#     path('login/', views.test),
+#     path('signup/', views.test),
+#     path('question/<int:id>/', views.test),
+#     path('ask/', views.test),
+#     path('popular/', views.test),
+#     path('new/', views.test),
+# ]
+urlpatterns = patterns('qa.views',
+   url(r'^$', 'test'),
+   url(r'^login/.*$', 'test', name='login'),
+   url(r'^signup/.*', 'test', name='signup'),
+   url(r'^question/(?P<id>[0-9]+)/$', 'test', name='question'),
+   url(r'^ask/.*', 'test', name='ask'),
+   url(r'^popular/.*', 'test', name='popular'),
+   url(r'^new/.*', 'test', name='new'),
+)
