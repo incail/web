385322ed2689f8a84853f25684f32e8c1a0fe0a7
Ilya Veremeev <vel.veremeev@gmail.com>
2023-01-18 18:15:24 +0300

    change urls

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -20,7 +20,7 @@ urlpatterns = [
     path('', views.test),
     path('login/', views.test),
     path('signup/', views.test),
-    path('question/123/', views.test),
+    path('question/<int:id>/', views.test),
     path('ask/', views.test),
     path('popular/', views.test),
     path('new/', views.test),
